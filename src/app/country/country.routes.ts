31e5f240d088619d44ce6bad9a5c41389900d88b
Carlos Verdugo <carlos.versa99@gmail.com>
2025-07-14 16:05:19 -0700

    Adding routes children

--- src/app/country/country.routes.ts
+++ src/app/country/country.routes.ts
@@ -1,10 +1,21 @@
 import { Routes } from '@angular/router';
 import { ByCapitalPageComponent } from './pages/by-capital-page/by-capital-page.component';
+import { CountryLayoutComponent } from './layouts/country-layout/country-layout.component';
 
 const countryRoutes: Routes = [
     {
         path: '',
-        component: ByCapitalPageComponent
+        component: CountryLayoutComponent,
+        children: [
+            {
+                path: 'by-capital',
+                component: ByCapitalPageComponent
+            },
+            {
+                path: '**',
+                redirectTo: 'by-capital',
+            }
+        ]
     }
 ]
 
