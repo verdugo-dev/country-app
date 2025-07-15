c16a759574c786bb20e8f666626de57c836c5502
Carlos Verdugo <carlos.versa99@gmail.com>
2025-07-15 16:46:03 -0700

    Adding rxResource

--- src/app/country/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/country/pages/by-capital-page/by-capital-page.component.ts
@@ -1,9 +1,10 @@
 import { Component, inject, resource, signal } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
 import { SerchInputComponent } from '../../components/serch-input/serch-input.component';
 import { CountryListComponent } from "../../components/country-list/country-list.component";
 import { CountryService } from '../../services/country.service';
 import type { Country } from '../../interfaces/country-interface';
-import { firstValueFrom } from 'rxjs';
+import { firstValueFrom, of } from 'rxjs';
 
 @Component({
   selector: 'app-by-capital-page',
@@ -16,27 +17,32 @@ export class ByCapitalPageComponent {
   countryService = inject( CountryService );
   query = signal('');
 
-  countryResource = resource({
-    request: () => ({ query: this.query() }),
-    loader: async({ request }) => {
-      if ( !request.query ) return [];
+  // countryResource = resource({
+  //   request: () => ({ query: this.query() }),
+  //   loader: async({ request }) => {
+  //     if ( !request.query ) return [];
 
-      return await firstValueFrom(
-        this.countryService.searchByCapital(request.query)
-      );
-    },
+  //     return await firstValueFrom(
+  //       this.countryService.searchByCapital(request.query)
+  //     );
+  //   },
+  // })
+
+  countryResource = rxResource({
+    request: () => ({ query: this.query() }),
+    loader: ({ request }) => {
+      if (!request.query) return of([]);
+      return this.countryService.searchByCapital(request.query);
+    }
   })
 
   // isLoading = signal(false);
   // isError = signal<string | null>(null)
   // countries = signal<Country[]>([])
-
   // onSearch( query: string ) {
   //   if ( this.isLoading() ) return;
-
   //   this.isLoading.set(true);
   //   this.isError.set(null);
-    
   //   this.countryService.searchByCapital( query )
   //     .subscribe({
   //       next: (countries) => {
