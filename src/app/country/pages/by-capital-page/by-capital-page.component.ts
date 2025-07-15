df50bd6d28d1c3772e682ea9bf451c8201456e47
Carlos Verdugo <carlos.versa99@gmail.com>
2025-07-15 16:06:53 -0700

    Adding catcherror

--- src/app/country/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/country/pages/by-capital-page/by-capital-page.component.ts
@@ -25,10 +25,16 @@ export class ByCapitalPageComponent {
     this.isError.set(null);
     
     this.countryService.searchByCapital( query )
-      .subscribe( countries => {
-        this.isLoading.set(false);
-        this.countries.set(countries);
-        console.log({ countries })
+      .subscribe({
+        next: (countries) => {
+          this.isLoading.set(false);
+          this.countries.set(countries);
+        },
+        error: ( err ) => {
+          this.isLoading.set(false);
+          this.countries.set([]);
+          this.isError.set(err);
+        }
       })
   }
 }
--- src/app/country/services/country.service.ts
+++ src/app/country/services/country.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { RESTCountry } from '../interfaces/rest-countries.interfaces';
-import { map, Observable } from 'rxjs';
+import { catchError, map, Observable, throwError } from 'rxjs';
 import type { Country } from '../interfaces/country-interface';
 import { CountryMapper } from '../mappers/country.mapper';
 
@@ -19,7 +19,12 @@ export class CountryService {
 
     return this.http.get<RESTCountry[]>(`${ API_URL }/capital/${ query}`)
       .pipe(
-        map( resp => CountryMapper.mapRestCountryArrayToCountryArray(resp) )
+        map(resp => CountryMapper.mapRestCountryArrayToCountryArray(resp)),
+        catchError(error => {
+          console.log('Error fetching ', error);
+
+          return throwError(() => new Error('No se pudo obtener países con ese query'))
+        })
       )
   }
 
