e9e6ebcc6b7f20c314bf6bc57a1027af779497c5
Carlos Verdugo <carlos.versa99@gmail.com>
2025-07-15 15:29:02 -0700

    Adding pipe decimal

--- src/app/country/components/country-list/country-list.component.ts
+++ src/app/country/components/country-list/country-list.component.ts
@@ -1,9 +1,10 @@
 import { Component, input } from '@angular/core';
 import type { Country } from '../../interfaces/country-interface';
+import { DecimalPipe } from '@angular/common';
 
 @Component({
   selector: 'country-list',
-  imports: [],
+  imports: [ DecimalPipe ],
   templateUrl: './country-list.component.html',
   styleUrl: './country-list.component.css'
 })
