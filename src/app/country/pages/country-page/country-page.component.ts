842f6fb5dee492eb72b06962069b4faddf6dbb5f
Carlos Verdugo <carlos.versa99@gmail.com>
2025-07-15 17:23:00 -0700

    Getting value by country

--- src/app/country/pages/country-page/country-page.component.ts
+++ src/app/country/pages/country-page/country-page.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+import { CountryService } from '../../services/country.service';
 
 @Component({
   selector: 'app-country-page',
@@ -7,5 +10,17 @@ import { Component } from '@angular/core';
   styleUrl: './country-page.component.css'
 })
 export class CountryPageComponent {
+
+  countryService = inject(CountryService);
+
+  countryCode = inject(ActivatedRoute).snapshot.params['code'];
+  // countryCode = inject(ActivatedRoute).snapshot.paramMap.get('code');
+
+  countryResource = rxResource({
+    request: () => ({ code: this.countryCode }),
+    loader: ({ request }) => {
+      return this.countryService.searchCountryByAlphaCode(request.code);
+    },
+  });
 
 }
