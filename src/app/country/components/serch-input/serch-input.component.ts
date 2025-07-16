e27814ce9794a745624e926a140b6daf714bb6cd
Carlos Verdugo <carlos.versa99@gmail.com>
2025-07-16 11:07:29 -0700

    Adding debounce

--- src/app/country/components/serch-input/serch-input.component.ts
+++ src/app/country/components/serch-input/serch-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, output } from '@angular/core';
+import { Component, effect, input, output, signal } from '@angular/core';
 
 @Component({
   selector: 'country-search-input',
@@ -10,5 +10,20 @@ export class SerchInputComponent {
 
   placeholder = input('Buscar');
   value = output<string>();
+  debounceTime = input(300);
+
+  inputValue = signal<string>('');
+
+  debounceEffect = effect((onCleanUp) => {
+    const value = this.inputValue();
+
+    const timeout = setTimeout(() => {
+      this.value.emit( value );
+    }, this.debounceTime());
+
+    onCleanUp(() => {
+      clearTimeout(timeout)
+    })
+  });
 
 }
--- src/app/country/services/country.service.ts
+++ src/app/country/services/country.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { RESTCountry } from '../interfaces/rest-countries.interfaces';
-import { catchError, delay, map, Observable, throwError } from 'rxjs';
+import { catchError, delay, map, Observable, of, throwError } from 'rxjs';
 import type { Country } from '../interfaces/country-interface';
 import { CountryMapper } from '../mappers/country.mapper';
 
@@ -20,7 +20,7 @@ export class CountryService {
     return this.http.get<RESTCountry[]>(`${ API_URL }/capital/${ query}`)
       .pipe(
         map(resp => CountryMapper.mapRestCountryArrayToCountryArray(resp)),
-        delay(2000),
+        // delay(2000),
         catchError(error => {
           console.log('Error fetching ', error);
 
